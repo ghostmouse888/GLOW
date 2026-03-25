c617047738da0c11c5a734ac834b2867bf043a8d
Nibaldo Cvitanic <n.cvitanic@icloud.com>
2026-03-25 12:06:04 -0700

    Initial Commit

---
+++ GLOW/GLOWApp.swift
@@ -0,0 +1,17 @@
+//
+//  GLOWApp.swift
+//  GLOW
+//
+//  Created by PC on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct GLOWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
